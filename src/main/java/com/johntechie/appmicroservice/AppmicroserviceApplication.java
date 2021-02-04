f33672ab8ab5559d70939a5559139aa04b169393
johngun9 <yohan.bs007@gmail.com>
2021-02-04 15:42:10 +0530

    intial commit

---
+++ src/main/java/com/johntechie/appmicroservice/AppmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.johntechie.appmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johntechie/appmicroservice/AppmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johntechie.appmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
