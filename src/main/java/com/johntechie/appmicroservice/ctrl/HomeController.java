11efce31e93a9b7bc9955c53e3dbcbaf681faca3
johngun9 <yohan.bs007@gmail.com>
2021-02-04 16:37:43 +0530

    added HomeController

---
+++ src/main/java/com/johntechie/appmicroservice/ctrl/HomeController.java
@@ -0,0 +1,18 @@
+package com.johntechie.appmicroservice.ctrl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String getGreeting(){
+        return "Hi Welcome to Home controller";
+    }
+
+    @GetMapping("/{name}")
+    public String getGreetingWithName(@PathVariable String name){
+        return "Hi "+name+" Welcome to Microservice Architecture Application";
+    }
+}
